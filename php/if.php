125d385e7680a32d5288a5063ec318f9432496c4
gdfaubion <gdfaubion@gmail.com>
2014-02-06 16:47:35 -0600

    Control Structures 1. 3 0f 5.
    If / else if / else

--- php/if.php
+++ php/if.php
@@ -4,45 +4,35 @@ $a = 5;
 $b = 10;
 $c = '10';
 
-if ($a < $b) {
+ if ($a < $b) {
 	
-	echo "$a is less than $b\n";
-}
+ 	echo "$a is less than $b\n";
 
-if ($b > $a) {
+} else {
 
-	echo "$b is greater than $a\n";
-}
+ 	echo "$a is greater than $b\n";
+  }
 
-if ($b >= $c) {
+ if ($b >= $c) {
 
-	echo "$b is greater than or equal to $c\n";
-}
+ 	echo "$b is greater than or equal to $c\n";
 
-if ($b <= $c) {
+} else {
 
-	echo "$b is less than or equal to $c\n";
-}
-
-if ($b == $c) {
- 	
- 	echo "$b is equal to $c\n";
+ 	echo "$b is less than or equal to $c\n";
 }
 
 if ($b === $c) {
-
+	
 	echo "$b is identical to $c\n";
-}
 
-if ($b != $c) {
+} else if ($b == $c) {
 
-	echo "$b is not equal to $c\n";
+	echo "$b is equal to $c\n";
 
-}
+} else {
 
-if ($b !== $c) {
-
-	echo "$b is not identical to $c\n";
+	echo "$b is not equal or identical to $c\n";
 }
 
 ?>
