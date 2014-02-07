729abfa105934266fc3734c917f5010e16133d88
gdfaubion <gdfaubion@gmail.com>
2014-02-07 17:07:20 -0600

    For loop part 2
    Added “What would you like to count by?”

--- php/for.php
+++ php/for.php
@@ -3,10 +3,17 @@
 fwrite(STDOUT, "Pick a starting number\n");
 
 $start = intval(fgets(STDIN));
+
 fwrite(STDOUT, "At what number should we stop\n");
 
 $stop = intval(fgets(STDIN));
 
-for ($i = $start; $i <= $stop; $i++) {
+fwrite(STDOUT, "What would you like to count by?\n");
+
+$increment_by = intval(fgets(STDIN));
+
+echo "Great! Lets count!";
+
+for ($i = $start; $i <= $stop; $i += $increment_by) {
 	echo "$i\n";
 }
