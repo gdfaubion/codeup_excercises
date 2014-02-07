5ae171df9eb83db995b227d031a3f3ba8330c20e
gdfaubion <gdfaubion@gmail.com>
2014-02-07 12:41:35 -0600

    High / Low 3
    This one actually works..

--- php/highlow.php
+++ php/highlow.php
@@ -1,28 +1,36 @@
 <?php
 // pick random # between 1 - 100
 
-$answer = mt_rand(1, 100);
+$answer = rand(1, 100);
 
 fwrite(STDOUT, 'Guess a number between 1 and 100 ');
 
+$num_of_guesses = 0;
 
 do {
 
 	$number = fgets(STDIN);
+	$num_of_guesses++;
 
 	if ($number < $answer) {
-		echo "HIGHER!";
-		echo "Guess again ";
+		echo "HIGHER!\n";
+		echo "Guess again \n";
 
 		} elseif ($number > $answer) {
-			echo "LOWER!";
-			echo "Guess again ";
+			echo "LOWER!\n";
+			echo "Guess again \n";
 
 		} else {
-			echo "WINNER!";
+			echo "WINNER!\n";
 		}
 
-} while ($number !== $answer);
+} while ($number != $answer);
+
+echo "Great job!!\n";
+
+echo "It took you $num_of_guesses guesses!\n";
+
+exit();
 
 
 
