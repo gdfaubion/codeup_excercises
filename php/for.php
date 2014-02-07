9f97cef8a1337efa265b06f5b70293b3c9fd99e1
gdfaubion <gdfaubion@gmail.com>
2014-02-07 17:48:51 -0600

    For Loop Part 3
    Added if statement incase of no input for the increment variable.

--- php/for.php
+++ php/for.php
@@ -12,8 +12,19 @@ fwrite(STDOUT, "What would you like to count by?\n");
 
 $increment_by = intval(fgets(STDIN));
 
-echo "Great! Lets count!";
+if($increment_by === 0) {
+
+	$increment_by = 1;
+}
+
+echo "Great! Lets count!\n";
 
 for ($i = $start; $i <= $stop; $i += $increment_by) {
 	echo "$i\n";
+
 }
+
+
+
+
+
