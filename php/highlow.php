a2839e6d6f84028d942269d7e5dc802a5c7bb89b
gdfaubion <gdfaubion@gmail.com>
2014-02-07 11:59:10 -0600

    High / Low Game

---
+++ php/highlow.php
@@ -0,0 +1,31 @@
+<?php
+// pick random # between 1 - 100
+
+$answer = rand(1, 100);
+
+fwrite(STDOUT, 'Guess a number between 1 and 100 ');
+
+
+do {
+
+	$number = fgets(STDIN);
+
+	if ($number < $answer) {
+		echo "HIGHER!";
+		echo "Guess again ";
+
+		} elseif ($number > $answer) {
+			echo "LOWER!";
+			echo "Guess again ";
+
+		} else {
+			echo "WINNER!";
+		}
+
+} while ($number !== $answer);
+
+
+
+
+
+
