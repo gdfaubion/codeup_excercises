f2413081383e16f4f7f6f31607774c8935f1a508
gdfaubion <gdfaubion@gmail.com>
2014-02-10 11:12:18 -0600

    foreach.3
    exercises part 3…

--- php/foreach.php
+++ php/foreach.php
@@ -3,9 +3,12 @@
 $things = array('Sgt. Pepper', "11", null, array(1, 2, 3,), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-	if(is_scalar($thing)) {
-		echo "{$thing} is a type of scalar\n";
+	if(is_scalar($thing)){
+		echo "{$thing} \n";
+	} elseif (is_array($thing)) {
+		echo "{$thing} \n";
+	} elseif (is_null($thing)) {
+		echo "{$thing} \n";
 	}
 }
-
 	  
