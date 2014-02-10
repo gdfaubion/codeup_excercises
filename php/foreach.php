4fef202efe12cdc6a47116249e645a775bc0319b
gdfaubion <gdfaubion@gmail.com>
2014-02-10 11:06:33 -0600

    foreach.2
    exercise part 2..

--- php/foreach.php
+++ php/foreach.php
@@ -3,22 +3,9 @@
 $things = array('Sgt. Pepper', "11", null, array(1, 2, 3,), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-	if (is_numeric($thing)) {
-		echo "{$thing} is a number\n";
-	} elseif (is_string($thing)) {
-		echo "{$thing} is a string\n";
-		} elseif (is_bool($thing)) {
-			echo "{$thing} is a boolean\n";
-		}
-} 
-
-foreach ($things as $thing) {
-	if (is_float($thing)) {
-		echo "{$thing} is a float\n";
-	} elseif (is_array($thing)) {
-		echo "{$thing} is an array\n";
-	} elseif (is_null($thing)) {
-		echo "{$thing} is null\n";
+	if(is_scalar($thing)) {
+		echo "{$thing} is a type of scalar\n";
 	}
 }
-  
+
+	  
