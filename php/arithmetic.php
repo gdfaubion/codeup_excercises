f44bcee5fe5bfaea88ca4130628dbde8e12e1346
gdfaubion <gdfaubion@gmail.com>
2014-02-11 11:10:33 -0600

    Functions
    Step 2 - 5.

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,17 +1,38 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+    echo $a + $b . "\n";
 }
 
 function subtract($a, $b) {
-    echo $a - $b;
+    echo $a - $b . "\n";
 }
 
 function multiply($a, $b) {
-    echo $a * $b;
+    echo $a * $b . "\n";
 }
 
 function divide($a, $b) {
-    echo $a / $b;
+    echo $a / $b . "\n";
 }
+
+function modulus($a, $b) {
+	echo $a % $b . "\n";
+}
+
+
+
+add(10, 15);
+
+subtract(35, 19);
+
+multiply(7, 6);
+
+divide(15, 5);
+
+modulus(10, 5);
+
+
+
+
+?>
