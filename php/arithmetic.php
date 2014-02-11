516c3f3cc47f923ca68e3c514e7e8f8a6c5f4e09
gdfaubion <gdfaubion@gmail.com>
2014-02-11 13:22:15 -0600

    Functions
    Steps 2 and 3

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,36 +1,61 @@
 <?php
 
-function add($a, $b) {
-    echo $a + $b . "\n";
+function add($a = 0, $b = 0) {
+	if(is_numeric($a) && is_numeric($b)) {
+		echo $a + $b . "\n";
+	} else {
+		echo "!!!ERROR!!! You have $a and $b, but both inputs must be numeric!\n";
+	}
 }
 
-function subtract($a, $b) {
-    echo $a - $b . "\n";
+function subtract($a = 0, $b = 0) {
+	if(is_numeric($a) && is_numeric($b)) {
+		echo $a - $b . "\n";
+	} else {
+		echo "!!!ERROR!!! You have $a and $b, but both inputs must be numeric!\n";
+	}   
 }
 
-function multiply($a, $b) {
-    echo $a * $b . "\n";
+function multiply($a = 1, $b = 1) {
+	if(is_numeric($a) && is_numeric($b)) {
+		echo $a * $b . "\n";
+	} else {
+		echo "!!!ERROR!!! You have $a and $b, but both inputs must be numeric!\n";
+	}   
 }
 
-function divide($a, $b) {
-    echo $a / $b . "\n";
+function divide($a = 1, $b = 1) {
+	if($a == 0 || $b == 0){
+		echo "!!!Error!!! Cannot divide by 0!\n";
+
+	}elseif(is_numeric($a) && is_numeric($b)) {
+		echo $a / $b . "\n";
+		
+	} else {
+		echo "!!!ERROR!!! You have $a and $b, but both inputs must be numeric!\n";
+	}   
 }
 
-function modulus($a, $b) {
-	echo $a % $b . "\n";
+function modulus($a = 0, $b = 0) {
+	if(is_numeric($a) && is_numeric($b)) {
+		echo $a % $b . "\n";
+	} else {
+		echo "!!!ERROR!!! You have $a and $b, but both inputs must be numeric!\n";
+	}
+	
 }
 
 
-
-add(10, 15);
+add(5);
 
 subtract(35, 19);
 
 multiply(7, 6);
 
-divide(15, 5);
+divide(15, 0);
 
-modulus(10, 5);
+modulus(10, 'John');
+
 
 
 
