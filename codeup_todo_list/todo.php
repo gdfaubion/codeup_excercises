61dd6b372f8ab878c8071fd8d4bdf7e351248c97
gdfaubion <gdfaubion@gmail.com>
2014-02-12 16:56:55 -0600

    Todo list
    Added new functions

--- codeup_todo_list/todo.php
+++ codeup_todo_list/todo.php
@@ -3,36 +3,59 @@
 // Create array to hold list of todo items
 $items = array();
 
-// The loop!
-do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {
-        // Display each item and a newline
-        $key++;
-        echo "[{$key}] {$item}\n";
+// List array items formatted for CLI
+function list_items($list) {
+    $return = '';
+    foreach ($list as $key => $value) {
+        $return .= "[" . ($key + 1) . "] " . $value . PHP_EOL;
+    } 
+        return $return;
     }
 
+// Get STDIN, strip whitespace and newlines, 
+// and convert to uppercase if $upper is true
+function get_input($upper = FALSE) 
+{
+    $input = trim(fgets(STDIN));
+    if ($upper) {
+        $input = strtoupper($input);
+    }
+    return $input;
+}
+
+// The loop!
+do {
+    // Echo the list produced by the function
+    echo list_items($items);
+
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+    echo '(N)ew item, (R)emove item, (S)ort from A-Z, (Z)Sort from Z-A, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = strtoupper(trim(fgets(STDIN)));
+    $input = get_input(TRUE);
 
     // Check for actionable input
     if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
+        $items[] = get_input();
     } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
-        $key = trim(fgets(STDIN));
+        $key = get_input();
         // Remove from array
-        $key--;
-        unset($items[$key]);
+        
+        unset($items[$key - 1]);
+    } elseif ($input == 'S') {
+        //sort $items from A-Z
+        sort($items);
+
+    } elseif ($input == 'Z') {
+        //sort from Z-A
+        rsort($items);
     }
 // Exit when input is (Q)uit
 } while ($input != 'Q');
