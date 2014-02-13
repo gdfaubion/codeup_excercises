f363394cba629e203af2f94e99db61b0e4c66398
gdfaubion <gdfaubion@gmail.com>
2014-02-13 10:47:21 -0600

    Array Search

--- php/search_arrays.php
+++ php/search_arrays.php
@@ -8,11 +8,13 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 // function checks if $names or $compare are arrays
 // Then searches through arrays
 function search_arrays($keys, $array = FALSE) {
-	$result = '';
+
 	if (is_array($array)) {
-		$result = in_array($keys, $array);
+		return array_search($keys, $array);
+	} else {
+		return FALSE;
 	}
-		return $result;
+	
 
 }
 
@@ -23,3 +25,34 @@ echo var_dump($result);
 $result = search_arrays('Bob', $names);
 
 echo var_dump($result);
+
+
+function match($needles, $haystack = FALSE) {
+
+	$num_matches = 0;
+
+	foreach ($needles as $needle) {
+		$result = array_search($needle, $haystack);
+
+		if(is_numeric($result)) {
+			$num_matches++;
+		}
+
+	}
+		return $num_matches;
+}
+
+
+$result = match($compare, $names);
+
+echo $result . PHP_EOL;
+
+var_dump($result);
+
+
+
+
+
+
+
+
