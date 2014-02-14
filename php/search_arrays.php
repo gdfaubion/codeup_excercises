249357d41e3d671e86d39d213fd570e07e0ce1ae
gdfaubion <gdfaubion@gmail.com>
2014-02-13 20:50:36 -0600

    Search Arrays

--- php/search_arrays.php
+++ php/search_arrays.php
@@ -1,53 +1,61 @@
 <?php
 
-// first names
-$names = ['Tina', 'Dana', 'Mike', "Amy", 'Adam'];
+// // first names
+// $names = ['Tina', 'Dana', 'Mike', "Amy", 'Adam'];
 
-$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+// $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-// function checks if $names or $compare are arrays
-// Then searches through arrays
-function search_arrays($keys, $array = FALSE) {
+// // function checks if $names or $compare are arrays
+// // Then searches through arrays
+// function search_arrays($keys, $array = FALSE) {
 
-	if (is_array($array)) {
-		return array_search($keys, $array);
-	} else {
-		return FALSE;
-	}
+// 	if (is_array($array)) {
+// 		return array_search($keys, $array);
+// 	} else {
+// 		return FALSE;
+// 	}
 	
 
-}
+// }
 
-$result = search_arrays('Tina', $names);
+// $result = search_arrays('Tina', $names);
 
-echo var_dump($result);
+// echo var_dump($result);
 
-$result = search_arrays('Bob', $names);
+// $result = search_arrays('Bob', $names);
 
-echo var_dump($result);
-
-
-function match($needles, $haystack = FALSE) {
-
-	$num_matches = 0;
-
-	foreach ($needles as $needle) {
-		$result = array_search($needle, $haystack);
-
-		if(is_numeric($result)) {
-			$num_matches++;
-		}
-
-	}
-		return $num_matches;
-}
+// echo var_dump($result);
 
 
-$result = match($compare, $names);
+// function match($needles, $haystack = FALSE) {
 
-echo $result . PHP_EOL;
+// 	$num_matches = 0;
 
-var_dump($result);
+// 	foreach ($needles as $needle) {
+// 		$result = array_search($needle, $haystack);
+
+// 		if(is_numeric($result)) {
+// 			$num_matches++;
+// 		}
+
+// 	}
+// 		return $num_matches;
+// }
+
+
+// $result = match($compare, $names);
+
+// echo $result . PHP_EOL;
+
+// var_dump($result);
+
+
+$array = (1, 2, 3, 4, 5);
+
+array_push($array, 6, 7, 8);
+
+print_r($array);
+
 
 
 
