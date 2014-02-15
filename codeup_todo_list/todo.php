8e2c97694883179534d733e9bbcb2605f5f01d1f
gdfaubion <gdfaubion@gmail.com>
2014-02-15 16:01:13 -0600

    Todo list

--- codeup_todo_list/todo.php
+++ codeup_todo_list/todo.php
@@ -23,13 +23,25 @@ function get_input($upper = FALSE)
     return $input;
 }
 
+//Create function that opens and reads a file,
+//returns the contents so we can add it the list of items
+function open_file() {
+    echo "Which file would you like to open?" . PHP_EOL;
+    $file = get_input();
+    $handle = fopen($file, "r");
+    $contents = fread($handle, filesize($file));
+    $contents_array = explode("\n", $contents);
+    fclose($handle);
+    return $contents_array;   
+}
+
 // The loop!
 do {
     // Echo the list produced by the function
     echo list_items($items);
 
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (S)ort from A-Z, (Z)Sort from Z-A, (O)pen file, (Q)uit : ';
+    echo '(N)ew item, (R)emove item, (A)sort from A-Z, (Z)Sort from Z-A, (O)pen file, (S)ave, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
@@ -38,44 +50,66 @@ do {
     // Check for actionable input
     if ($input == 'N') {
         // Ask for entry
-        echo 'Enter item: ';
-        // Add entry to list array
-        $items[] = get_input();
-        if(count($items) > 1) {
-            echo "Would you like to add this item to the (B)eggining or (E)nd of your list?\n";
-            $input = get_input(TRUE);
-        }elseif($input == 'B') {
-            array_unshift($items, $input);
+        echo "Would you like to add this item to the (B)eggining or (E)nd of your list?\n";
+        $input = get_input(TRUE);
+        if($input == 'B') {
+            echo "Enter new item: ";
+            $new_input = get_input();
+            array_unshift($items, $new_input);
         } elseif($input == 'E') {
-            array_push($itmes, $input);
-            }
-    } elseif ($input == 'R') {
+            echo "Enter new item: ";
+            $new_input = get_input();
+            array_push($items, $new_input);
+        }
+    }elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
         $key = get_input();
         // Remove from array
         unset($items[$key - 1]);
-    } elseif ($input == 'S') {
+    } elseif ($input == 'A') {
         //sort $items from A-Z
         sort($items);
     } elseif ($input == 'Z') {
         //sort from Z-A
         rsort($items);
-    }  elseif ($input == 'F') {
+    } elseif ($input == 'F') {
+        //removes the first item from the list
         array_shift($items);
     } elseif ($input == 'L') {
-        array_pop($itmes);
-    } elseif($input = 'O') {
-        echo "Enter file name" . PHP_EOL;
-        $which_file = get_input();
-        $handle = fopen($which_file, "r");
-        $contents = fread($handle, filesize($which_file));
-        fclose($handle);
-        $items = explode("\n", $contents); 
-}
-// Exit when input is (Q)uit
-} while ($input != 'Q');
+        //removes the last item from the list
+        array_pop($items);
+    } elseif($input == 'O') {
+        $file = open_file();
+        foreach($file as $line) {
+            if ($line != '') {
+                $items[] = $line;
+            }
+        }
+    } elseif($input == 'S') {
+        $filename = "data/todo_list.txt";
+        if(is_file($filename)) {
+            echo "This file already exists would you like to continue? (Y)es or (N)o " . PHP_EOL;
+            $input = get_input(TRUE);
+                if($input = 'Y') {
+                    $handle = fopen($filename, 'w');
+                    foreach ($items as $item) {
+                    fwrite($handle, PHP_EOL . $item);
+                    }
+                    fclose($handle);
+                    echo "Your save was successful!" . PHP_EOL;
+                    
+                } elseif($input == 'N') {
+                    return FALSE;
+                }
+        
+        }
+        
+    }
+
+}   // Exit when input is (Q)uit
+    while ($input != 'Q');
 
 // Say Goodbye!
 echo "Goodbye!\n";
