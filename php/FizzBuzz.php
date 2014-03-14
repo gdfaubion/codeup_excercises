86aeb7fbef036cfe67c7c7255c54d8d9c062d498
gdfaubion <gdfaubion@gmail.com>
2014-03-13 20:34:45 -0500

    Refactored for a cleaner look

--- php/FizzBuzz.php
+++ php/FizzBuzz.php
@@ -3,18 +3,31 @@
 //if % by 3 echo Fizz
 //if % by 5 echo Buzz
 //if % by both 3 & 5 echo FizzBuzz
-for ($i = 1; $i <= 100; $i++) {
+// for ($i = 1; $i <= 100; $i++) {
 
-	if ($i % 3 == 0 && $i % 5 == 0) {
-		echo "FizzBuzz\n";
+// 	if ($i % 3 == 0 && $i % 5 == 0) {
+// 		echo "FizzBuzz\n";
 
-		} elseif ($i % 3 == 0) {
-			echo "Fizz\n";
+// 		} elseif ($i % 3 == 0) {
+// 			echo "Fizz\n";
 
-			} elseif ($i % 5 == 0) {
-				echo "Buzz\n";
+// 			} elseif ($i % 5 == 0) {
+// 				echo "Buzz\n";
 
-				} else {
-					echo "$i\n";
-			}
+// 				} else {
+// 					echo "$i\n";
+// 			}
+// }
+
+
+for($i = 1; $i <= 100; $i++) {
+	if($i % 3 === 0 && $i % 5 === 0) {
+		echo "FizzBuzz" . PHP_EOL;
+	} elseif ($i % 3 === 0) {
+		echo "Fizz" . PHP_EOL;
+	} elseif ($i % 5 === 0) {
+		echo "Buzz" . PHP_EOL;
+	} else {
+		echo $i . PHP_EOL;
+	}
 }
