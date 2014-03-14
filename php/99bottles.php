0cdbb5a135893662444b05fe07128b48ecc9a4cb
gdfaubion <gdfaubion@gmail.com>
2014-03-13 20:37:12 -0500

    Refactored for cleaner code

--- php/99bottles.php
+++ php/99bottles.php
@@ -13,5 +13,5 @@ for($bottles = 99; $bottles >= 0; $bottles--) {
 			echo "$bottles bottles of beer on the wall, $bottles bottles of beer! Take one down pass it around, $bottles2 bottles of beer on the wall!" . PHP_EOL;
 		}
 
-}
+	}
 ?>
